d4fdcb0bfb26057ca6f02667417fd83ba46dde70
chortlezs <chortlezs@example.com>
2026-08-28 15:48:58 +0800

    Fix Vercel deployment paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Embodied_data_storytelling_gallery/',
   build: {
     sourcemap: 'hidden',
   },
